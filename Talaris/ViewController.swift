ae7702d43308ca4c6925ba7e293a3fb01a09d32c
Taha Baig <tahabaig@tahas-macbook-pro.local>
2019-01-14 12:43:54 -0500

    Initial Commit

---
+++ Talaris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Talaris
+//
+//  Created by Taha Baig on 1/14/19.
+//  Copyright © 2019 Talaris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
