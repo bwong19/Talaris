c7289c09048560017c97c25d73fbe4e10a49f407
Taha Baig <tahabaig@tahas-macbook-pro.local>
2019-01-14 12:58:21 -0500

    Setup navigation in AppDelegate and created LoginViewController

--- Talaris/AppDelegate.swift
+++ Talaris/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let navController = UINavigationController(rootViewController: LoginViewController())
+        window?.rootViewController = navController
+        
         // Override point for customization after application launch.
         return true
     }
