8468367cd1de8d997f807836d33055b30e051628
Talaris Health <talarishealth@Talariss-Air.frontierlocal.net>
2019-06-21 10:28:36 -0700

    changed speech synthesizer

--- Controllers/GaitTestControllers/TUGViewController.swift
+++ Controllers/GaitTestControllers/TUGViewController.swift
@@ -11,7 +11,7 @@ import CoreMotion
 import AVFoundation
 
 // Timed Up and Go Gait Test
-class TUGViewController: GaitTestViewController {
+class TUGViewController: GaitTestViewController, AVSpeechSynthesizerDelegate {
     private let SAMPLING_RATE = 10.0
     private let STANDING_THRESHOLD = 1.0
     private let SITTING_THRESHOLD = 0.4
@@ -22,10 +22,18 @@ class TUGViewController: GaitTestViewController {
     private var hasStoodUp: Bool
     private var walking: Bool
     
+    private let synthesizer = AVSpeechSynthesizer()
+    private let didFinish = false
+    private let soundCode = 1005
+    private var numUtterances = 0
+    private var totalUtterances = 0
+    
+    private var speakTime: Double;
+    
     init() {
         sit2stand = 0.0
         stand2sit = 0.0
-        
+        speakTime = 0.0
         hasStoodUp = false
         walking = false
         
@@ -54,27 +62,52 @@ class TUGViewController: GaitTestViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        synthesizer.delegate = self
+        UIApplication.shared.isIdleTimerDisabled = true
         
-        PhoneVoice.speak(speech: "Place the phone in your pocket")
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5), execute: {
-            self.startTest()
-        })
+        self.startTest()
         
     }
     
     override func startTest() {
         super.startTest()
-        PhoneVoice.speak(speech: "Start walking")
+        synthesizer.speak(getUtterance("Please sit down. Then, please secure your phone to your waist using the provided phone clip. Once the phone is secured, please sit still for at least 5 seconds. Please press “REPEAT” to repeat the instructions"))
+        
+        synthesizer.speak(getUtterance("On the words BEGIN WALKING, you will stand up, walk to the 3-meter mark, turn around, walk back towards the chair. and sit down. Walk at your regular pace."))
+        
+        speakTime = self.counter
+        
+        synthesizer.speak(getUtterance("start walking"))
     }
     
     override func stopTest() {
         super.stopTest()
-        PhoneVoice.speak(speech: "Good Work!")
+        synthesizer.speak(getUtterance("Good Work!"))
         
-        let resultsDict  = ["tug_time" : counter, "sit_to_stand_time" : sit2stand]
-        let cv = CheckViewController(message: String(format: "Your TUG time was %.1lf seconds. Your sit-to-stand duration is %.1lf seconds", counter, sit2stand), resultsDict : resultsDict as Dictionary<String, Any>, motionTracker:self.motionTracker, testType: "TUG")
+        let resultsDict  = ["tug_time" : counter-speakTime, "sit_to_stand_time" : sit2stand]
+        let cv = CheckViewController(message: String(format: "Your TUG time was %.1lf seconds. Your sit-to-stand duration is %.1lf seconds", counter-speakTime, sit2stand), resultsDict : resultsDict as Dictionary<String, Any>, motionTracker:self.motionTracker, testType: "TUG")
         self.navigationController!.pushViewController(cv, animated: true)
+    }
+    
+    func getUtterance(_ speech: String) -> AVSpeechUtterance {
+        let utterance = AVSpeechUtterance(string: speech)
+        utterance.rate = 0.4
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.postUtteranceDelay = 1
+        totalUtterances += 1
+        return utterance
+    }
+    
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        numUtterances += 1
+        if (numUtterances == totalUtterances) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                PhoneVoice.speak(speech: "Begin")
+                AudioServicesPlaySystemSound(SystemSoundID(self.soundCode));
+                super.startTest()
+            }
+        }
     }
 
 }
